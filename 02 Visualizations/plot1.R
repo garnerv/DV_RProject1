21e2b25a31f89d8d221ab3c597b3d6fb57758cdf
Garner Vincent <garnervincent@gmail.om>
2015-09-12 17:59:28 -0500

    Wrote first 3 plot analyses

--- 02 Visualizations/plot1.R
+++ 02 Visualizations/plot1.R
@@ -2,6 +2,8 @@ require("jsonlite")
 require("RCurl")
 # Change the USER and PASS below to be your UTEid
 nullDf <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_gv4353', PASS='orcl_gv4353', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+summary(nullDf)
+head(nullDf)
 
 
 require(extrafont)
@@ -10,9 +12,6 @@ ggplot() +
   coord_cartesian() + 
   scale_x_continuous() +
   scale_y_continuous() +
-  #facet_wrap(~SURVIVED) +
-  #facet_grid(.~SURVIVED, labeller=label_both) + # Same as facet_wrap but with a label.
-  #facet_grid(PCLASS~SURVIVED, labeller=label_both) +
   labs(title='Titanic') +
   labs(x="Age", y=paste("Fare")) +
   layer(data=nullDf, 
@@ -21,6 +20,5 @@ ggplot() +
         stat_params=list(), 
         geom="point",
         geom_params=list(), 
-        #position=position_identity()
         position=position_jitter(width=0.3, height=0)
   )
